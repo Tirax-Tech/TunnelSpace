14b418b99467eafdca93f441232edf7c201cfe1e
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-04-09 08:38:26 +0700

    Create Connection Info Panel control instead of the connection buttons

---
+++ src/Tirax.TunnelSpace/ViewModels/ConnectionInfoPanelViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using ReactiveUI;
+using Tirax.TunnelSpace.Domain;
+
+namespace Tirax.TunnelSpace.ViewModels;
+
+public sealed class TunnelConfigToConnectionInfoPanel : IValueConverter
+{
+    public static TunnelConfigToConnectionInfoPanel Instance { get; } = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is TunnelConfig tunnelConfig ? new ConnectionInfoPanelViewModel(tunnelConfig) : null;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is ConnectionInfoPanelViewModel viewModel ? viewModel.Model : null;
+}
+
+public sealed class ConnectionInfoPanelViewModel : ViewModelBase
+{
+    TunnelConfig tunnelConfig;
+
+    string name = "(Sample name)";
+
+    [DesignOnly(true)]
+    public ConnectionInfoPanelViewModel() : this(new(Guid.Empty, "(Sample Host)", 9999, 8888, "(Sample Remote Host)", 7777, "(Sample Name)")) { }
+
+    public ConnectionInfoPanelViewModel(TunnelConfig tunnelConfig)
+    {
+        this.tunnelConfig = tunnelConfig;
+        Name = tunnelConfig.Name;
+    }
+
+    public string Name {
+        get => name;
+        set => this.RaiseAndSetIfChanged(ref name, value);
+    }
+
+    public TunnelConfig Model {
+        get => tunnelConfig;
+        set => this.RaiseAndSetIfChanged(ref tunnelConfig, value);
+    }
+}
---
+++ src/Tirax.TunnelSpace/Views/ConnectionInfoPanelView.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Tirax.TunnelSpace.Views;
+
+public partial class ConnectionInfoPanelView : UserControl
+{
+    public ConnectionInfoPanelView() {
+        InitializeComponent();
+    }
+}
