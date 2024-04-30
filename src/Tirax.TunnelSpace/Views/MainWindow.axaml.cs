41e8081713eed2215f1b07e3573eecac11248d3a
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-05-01 06:05:11 +0700

    Fix release build failure

--- src/Tirax.TunnelSpace/Views/MainWindow.axaml.cs
+++ src/Tirax.TunnelSpace/Views/MainWindow.axaml.cs
@@ -6,6 +6,10 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+#if DEBUG
         InitializeComponent(attachDevTools: true);
+#else
+        InitializeComponent();
+#endif
     }
 }
